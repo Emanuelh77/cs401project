a8779d1ba3c5a4bbc6708d9b8e4ff3fde29eaa48
Emanuel Hernandez <emanuelh77@hotmail.com>
2020-02-19 14:51:01 -0700

    updated website

--- reviews.php
+++ reviews.php
@@ -3,18 +3,18 @@
                 <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
         </head>
         <header>
-                <div class="split-left">
+                <div class="splitleft">
                         <title>TCET</title>
                         <img src="TCET-logo.png" alt="TCET logo">
         
                         <nav>
-                                <a class="active" href="index.html">Home</a> |
-                                <a href="reviews.html">Reviews</a> |
+                                <a href="index.html">Home</a> |
+                                <a class="active" href="reviews.html">Reviews</a> |
                                 <a href="suggestions.html">Suggestions</a> |
                                 <a href="premium.html">Premium</a>
                         </nav>
                 </div>
-                <div class="split-right">
+                <div class="splitright">
                         <div class="topcorner">
                           <p>Log in</p>
                           <p>Sign up</p>
